d64ea7a22070a73a9ad8cf05b54362ec0d000d8a
Dhananjay Tupe <77236557+Djay-ui@users.noreply.github.com>
2026-08-19 14:38:56 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ const REPORTS = [
       "Integrated OSSEC for intrusion detection, alerting and log analysis during testing",
       "Documented risk severity, exploitation steps and remediation for every finding",
     ],
-    tools: ["DVWA", "Burp Suite", "OWASP ZAP", "OSSEC", "Ubuntu", "Kali", "Metaspolite"],
+    tools: ["DVWA", "Burp Suite", "OWASP ZAP", "OSSEC", "Ubuntu"],
     status: "Report delivered",
   },
   {
@@ -44,7 +44,7 @@ const REPORTS = [
     ],
     tools: ["AWS", "Jenkins", "SonarQube", "Docker", "Kubernetes", "Trivy", "Grafana"],
     status: "Live write-up",
-    link: https://medium.com/@dhananjaytupe748/devsecops-netflix-clone-ci-cd-with-monitoring-email-part-1-36e2fe9bbb6f, // TODO: paste your Medium article URL here, e.g. "https://medium.com/@you/netflix-clone-secure-ci-cd-pipeline"
+    link: null, // TODO: paste your Medium article URL here, e.g. "https://medium.com/@you/netflix-clone-secure-ci-cd-pipeline"
     linkLabel: "Read the write-up",
   },
 ];
